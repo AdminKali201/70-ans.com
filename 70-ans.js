ab071adfcc406ee06a665b8c01a8abd898c0a5eb
BOU LAROU <105955963+AdminKali201@users.noreply.github.com>
2023-09-10 14:57:45 +0100

    Add files via upload

---
+++ 70-ans.js
@@ -0,0 +1,11 @@
+const header = document.getElementsByClassName("header");
+const toggleClass = "is-sticky";
+
+document.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > 150) {
+    header.classList.add(toggleClass);
+  } else {
+    header.classList.remove(toggleClass);
+  }
+});
